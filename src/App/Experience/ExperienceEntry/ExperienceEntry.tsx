30356755c1813aaca5ed94ff86d237fc7c18e9c6
Elijah <nguy2865@gmail.com>
2025-11-12 00:17:40 -0600

    style experience list when there's more than one entry. add experience

--- src/App/Experience/ExperienceEntry/ExperienceEntry.tsx
+++ src/App/Experience/ExperienceEntry/ExperienceEntry.tsx
@@ -6,11 +6,10 @@ interface ExperienceEntryProps {
 }
 
 function ExperienceEntry({ experience }: ExperienceEntryProps)  {
-
   return <>
     <ul className="experience-container">
-      {experience.map((exp) => (
-        <div className="experience" key={exp.Title}>
+      {experience.map((exp, index) => (
+        <div className={`experience ${index % 2 == 1 ? 'experience-alt' : ''}`} key={exp.Title}>
           <div className="experience-text">
             <div className="experience-location">{exp.Location}: {exp.Duration}</div>
             <div className="experience-title">{exp.Title}</div>
